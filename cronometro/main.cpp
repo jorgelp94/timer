ec15430a1bb74da6fedeb9ce789e30a567da1aa0
Jorge Luis Perales <jorgelp94@MacBook-Pro-de-Jorge.local>
2016-01-30 16:04:07 -0600

    Empty

---
+++ cronometro/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cronometro
+//
+//  Created by Jorge Luis Perales on 30/01/16.
+//  Copyright © 2016 Jorge Luis Perales. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
